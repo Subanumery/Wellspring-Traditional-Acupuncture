043807cae69844953ab807d95b0d9e97b7498500
Subanumery <subanumery@gmail.com>
2026-08-25 18:28:44 +0100

    footer and header in different files now

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-document.getElementById("menu_icon").addEventListener("click", toggleMenu);
-    
-  function toggleMenu() {
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+
+        document.getElementById("header").innerHTML = data;
+        document.getElementById("menu_icon").addEventListener("click", toggleMenu);
+
+    })
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+      
+        document.getElementById("footer").innerHTML = data;
+
+    })
+
+
+function toggleMenu() {
 
     const popoutMenu = document.getElementById("popout_menu");
     const menuIcon = document.getElementById("menu_icon");
@@ -10,26 +26,26 @@ document.getElementById("menu_icon").addEventListener("click", toggleMenu);
 
     if (screenWidth <= 1250) {
 
-      if (popoutMenu.style.left === "100%") {
+        if (popoutMenu.style.left === "100%") {
+
+            popoutMenu.style.left = "0%";
+            menuIcon.classList.add("fa-times");
+            menuIcon.classList.remove("fa-bars");
+            footer.classList.add("sticky_footer");
+            body.classList.add("noscroll");
+
+            popoutMenu.scrollTop = 0;
+
+        }
         
-        popoutMenu.style.left = "0%";
-        menuIcon.classList.add("fa-times");
-        menuIcon.classList.remove("fa-bars");
-        footer.classList.add("sticky_footer");
-        body.classList.add("noscroll");
-        popoutMenu.scrollTop(0);
+        else {
 
+            popoutMenu.style.left = "100%";
+            menuIcon.classList.add("fa-bars");
+            menuIcon.classList.remove("fa-times");
+            footer.classList.remove("sticky_footer");
+            body.classList.remove("noscroll");
 
-      }
-      
-      else {
-
-        popoutMenu.style.left = "100%";
-        menuIcon.classList.add("fa-bars");
-        menuIcon.classList.remove("fa-times");
-        footer.classList.remove("sticky_footer");
-        body.classList.remove("noscroll");
-
-      }
+        }
     }
-  }
+}
